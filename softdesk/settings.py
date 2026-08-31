f61e30050d6514aa8584540c4d57136a108b608b
Kevin Delcroix <kevin.delcroix@outlook.fr>
2026-08-31 17:27:55 +0200

    essai pagination

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     # Configuration de la pagination pour le Green Code
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,  # Nombre d'éléments par page
+    "PAGE_SIZE": 5,  # Nombre d'éléments par page
 }
 
 SIMPLE_JWT = {
